eb85c08a0e95e54fe554bc7793323474d8f1e1ca
Devon Cash <devon.p.cash@gmail.com>
2024-07-10 11:06:30 -0700

    Added all users access to admin panel, USER CTL IMPORTANT

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Post::class);
     }
+
+    public function canAccessFilament()
+    {
+        return true;
+    }
 }
