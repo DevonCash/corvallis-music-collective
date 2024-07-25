7ba8a306b1f3479bc1025212f0162078a6878c61
Devon Cash <devon.p.cash@gmail.com>
2024-07-25 14:51:54 -0700

    for real this time

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,11 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\HandleInertiaRequests;
 return Application::configure(basePath: dirname(__DIR__))
-    ->booted(function () {
-        if ($this->app->environment("production")) {
-            \URL::forceScheme("https");
-        }
-    })
+
     ->withRouting(
         web: __DIR__ . "/../routes/web.php",
         commands: __DIR__ . "/../routes/console.php",
