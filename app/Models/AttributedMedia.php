3412c87c3b300ad765c56d7bd16ef4952b944cd9
Devon Cash <devon.p.cash@gmail.com>
2024-08-21 12:12:18 -0700

    Trying default name

--- app/Models/AttributedMedia.php
+++ app/Models/AttributedMedia.php
@@ -9,6 +9,16 @@ class AttributedMedia extends Media
     protected $table = "attributed_media";
     protected $fillable = ['path'];
 
+
+    protected static function boot(){
+        parent::boot();
+        static::creating( function ($model) {
+            if (empty($model->name)) {
+                $model->name = $model->file_name;
+            }
+        });
+    }
+
     public function events()
     {
         return $this->hasMany(Event::class, "poster_id");
